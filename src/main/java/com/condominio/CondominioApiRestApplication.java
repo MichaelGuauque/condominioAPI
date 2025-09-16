3475b4a902fd74acfaaea1ac0e4b3949a43d5b26
MichaelGuauque <mcalejo98@gmail.com>
2025-09-15 21:43:28 -0500

    Creacion proyecto

---
+++ src/main/java/com/condominio/CondominioApiRestApplication.java
@@ -0,0 +1,13 @@
+package com.condominio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CondominioApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CondominioApiRestApplication.class, args);
+	}
+
+}
